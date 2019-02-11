cb091a13fafd2cc2230c83439665bf7b60546329
Tyler Zhao <tylerzhaoprogrammer@gmail.com>
2019-02-11 17:56:26 -0500

    initial commit for swift version

---
+++ 20190211-TylerZhao-NYCSchools-Swift/20190211-TylerZhao-NYCSchools-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190211-TylerZhao-NYCSchools-Swift
+//
+//  Created by Tyler Zhao on 2/11/19.
+//  Copyright © 2019 Tyler Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 20190211-TylerZhao-NYCSchools-Swift/20190211-TylerZhao-NYCSchools-SwiftTests/_0190211_TylerZhao_NYCSchools_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  _0190211_TylerZhao_NYCSchools_SwiftTests.swift
+//  20190211-TylerZhao-NYCSchools-SwiftTests
+//
+//  Created by Tyler Zhao on 2/11/19.
+//  Copyright © 2019 Tyler Zhao. All rights reserved.
+//
+
+import XCTest
+@testable import _0190211_TylerZhao_NYCSchools_Swift
+
+class _0190211_TylerZhao_NYCSchools_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
